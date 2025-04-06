c6201c8b47d06d84d6017fff0c0a4f80947b6b1c
Ayuik <marcosayelen@gmail.com>
2025-04-06 08:18:49 +0200

    feat: Homepage component created. Route built

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-
 import './App.css'
+import HomePage from './pages/HomePage'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default App
