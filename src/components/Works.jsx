5f0e141b09faed608ab9676ec143384e33f687e4
Ayuik <marcosayelen@gmail.com>
2025-04-06 17:15:00 +0200

    fix: changed id into id_work to match db

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ function Works() {
 //Solo para probar
   const works = [
     {
-      work_id: 1,
+      id_work: 1,
       work_tittle: "Proyecto 1",
       work_link:"",
       work_description: "Descripción del proyecto 1",
@@ -12,7 +12,7 @@ function Works() {
       ],
     },
     {
-      work_id: 3,
+        id_work: 3,
       work_tittle: "Proyecto 2",
       work_link:"",
       work_description: "Descripción del proyecto 2",
@@ -22,7 +22,7 @@ function Works() {
       ],
     },
     {
-        work_id: 4,
+        id_work: 4,
         work_tittle: "Proyecto 2",
         work_link:"",
         work_description: "Descripción del proyecto 2",
@@ -32,7 +32,7 @@ function Works() {
         ],
       },
       {
-        work_id: 5,
+        id_work: 5,
         work_tittle: "Proyecto 2",
         work_link:"",
         work_description: "Descripción del proyecto 2",
@@ -42,7 +42,7 @@ function Works() {
         ],
       },
       {
-        work_id: 6,
+        id_work: 6,
         work_tittle: "Proyecto 2",
         work_link:"",
         work_description: "Descripción del proyecto 2",
@@ -60,14 +60,14 @@ function Works() {
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {works.map((work) => (
           <div
-            key={work.work_id}
+            key={work.id_work}
             className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow"
           >
             <div className="p-6">
               <h3 className="font-bold text-lg mb-2">{work.work_tittle}</h3>
               <p className="text-gray-700 mb-4">{work.work_description}</p>
               <a
-                href={work_link}
+                href={work.work_link}
                 className="px-2 py-1 bg-purple-500 text-white rounded-md hover:bg-purple-600 transition-colors inline-block"
               >
                 Ver proyecto
