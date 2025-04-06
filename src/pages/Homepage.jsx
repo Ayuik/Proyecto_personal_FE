673aa8d1460737e85c335897d81a9e590e569965
Ayuik <marcosayelen@gmail.com>
2025-04-06 09:11:26 +0200

    feat: Home component created

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,12 @@
 import Header from "../components/Header"
+import Home from "../components/Home"
 function HomePage() {
     return (
         <div className="min-h-screen">
             <Header />
+            <section className="py-20 bg-gradient-to-b from-black to-gray-900 text-white">
+                <Home />
+            </section>
         </div>
     );
 }
