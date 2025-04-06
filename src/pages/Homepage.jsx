4d3647a7bc4a2e1b231afc99b1cf16fd3e77c741
Ayuik <marcosayelen@gmail.com>
2025-04-06 09:36:14 +0200

    feat: About component created

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,7 @@
 import Header from "../components/Header"
 import Home from "../components/Home"
+import About from "../components/About"
+
 function HomePage() {
     return (
         <div className="min-h-screen">
@@ -7,6 +9,9 @@ function HomePage() {
             <section className="py-20 bg-gradient-to-b from-black to-gray-900 text-white">
                 <Home />
             </section>
+            <section id="about" className="py-20 bg-gray-100">
+                <About />
+            </section>          
         </div>
     );
 }
